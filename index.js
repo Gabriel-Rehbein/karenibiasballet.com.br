4f45a38a03a302a6af288f637d5555d32efb22c8
gabriel-rehbein <gabrielmr0812@gmail.com>
2025-07-23 19:48:07 -0300

    versao 5.0

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 
-const menuToggle = document.getElementById("menuToggle");
-const menuLista = document.getElementById("menuLista");
+  function toggleMenu() {
+    const menu = document.querySelector('.menu-list');
+    const button = document.querySelector('.menu-toggle');
 
-menuToggle.addEventListener("click", () => {
-    menuLista.classList.toggle("ativo");
-});
-
-
-let index = 0;
-const banners = document.querySelectorAll('.banner-img');
-
-setInterval(() => {
-    banners.forEach((img, i) => {
-        img.classList.remove('active');
-        if (i === index) img.classList.add('active');
-    });
-
-    index = (index + 1) % banners.length;
-}, 4000);
-
+    if (menu.classList.contains('ativo')) {
+      menu.classList.remove('ativo');
+      button.textContent = '☰';
+    } else {
+      menu.classList.add('ativo');
+      button.textContent = '✖';
+    }
+  }
